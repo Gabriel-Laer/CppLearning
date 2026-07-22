d65452227d416771310f3e409607538625176753
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-22 15:43:57 -0300

    first code

---
+++ basics/helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std; // permite usar as keyword da std sem uso de std::***
+
+int main(){
+
+    // std::cout é mais comum em programas maiores
+    cout << "Hello World!";
+    return 0;
+}
