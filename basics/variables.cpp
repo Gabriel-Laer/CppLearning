4c3b7d419bde3f145ee9b6e2cd0144c7d74a8126
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-23 08:17:50 -0300

    Primeiras variaveis em c++

---
+++ basics/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int x = 28;
+    double y = 29;
+    char a = 'A';
+    std::string b = "ABC";
+    bool c = true;
+
+    std::cout << x << std::endl << y << std::endl << a << std::endl << b << std:: endl << c << std::endl;
+    return 0;
+}
