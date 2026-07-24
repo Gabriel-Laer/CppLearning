3c8d04661e8b7995f25efd588a48553debce2557
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-24 09:07:53 -0300

    string teste com e sem append()

--- basics/string.cpp
+++ basics/string.cpp
@@ -2,6 +2,13 @@
 #include <string>
 
 int main(){
-    
+    std::string firstName = "Gabriel";
+    std::string lastName = "Marques";
+    std::string fullName = "7";
+    //concatenação manual
+    std::cout << firstName + " " + lastName << '\n';
+    // concatenação usando append()
+    fullName = lastName.append(firstName);
+    std::cout << fullName << std::endl;
     return 0;
 }
