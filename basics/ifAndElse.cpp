c466af6b8d5273973b51c0afc2d931fdb2360f46
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-25 09:12:25 -0300

    testando if e else

--- basics/ifAndElse.cpp
+++ basics/ifAndElse.cpp
@@ -2,5 +2,17 @@
 #include <string>
 
 int main(){
+    int x = 10;
+    int y = 20;
+    std::string great = "x is great than y";
+    std::string less = "x is less than y";
+
+    if (x > y)
+    {
+        std::cout << great << std::endl;
+    }else {
+        std::cout << less << std::endl;
+    }
+    
     return 0;
 }
