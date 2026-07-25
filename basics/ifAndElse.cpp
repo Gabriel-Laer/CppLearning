09b435090039b14f8d6552603612b5beaee85a13
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-25 09:17:37 -0300

    adicionando operador ternario

--- basics/ifAndElse.cpp
+++ basics/ifAndElse.cpp
@@ -4,6 +4,7 @@
 int main(){
     int x = 10;
     int y = 20;
+    std::string test = (x > y) ? "yes x is great than y" : "no x is less than y";
     std::string great = "x is great than y";
     std::string less = "x is less than y";
 
@@ -14,5 +15,7 @@ int main(){
         std::cout << less << std::endl;
     }
     
+    std::cout << test << std::endl;
+
     return 0;
 }
