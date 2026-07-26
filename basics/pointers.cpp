7c731ef825baac7c7eaf07e5bbf9ee451c60591c
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-26 09:29:16 -0300

    teste com pointers

--- basics/pointers.cpp
+++ basics/pointers.cpp
@@ -3,8 +3,10 @@
 
 int main(){
     int numberOrign = 10;
-    int &numberCopy = numberOrign;
+    int &numberCopy = numberOrign;// reference é um apelido(alias) para uma variavel existente, so pode ser definada desse modo
+    int* pointer = &numberCopy;// definindo um pointer, nele se armazena endereço de memoria
 
-    std::cout << numberCopy << std::endl;
+    std::cout << "Usando Reference variable " << numberCopy << std::endl;
+    std::cout << "Endereço de memoria de numberOrign: " << pointer << std::endl;
     return 0;
 }
