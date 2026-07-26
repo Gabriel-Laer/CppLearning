00ba6cfb475acb95ff544f15c9371d51c63f5dc8
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-26 08:34:00 -0300

    teste em arrays

--- basics/arrays.cpp
+++ basics/arrays.cpp
@@ -2,6 +2,10 @@
 #include <string>
 
 int main(){
-    std::string hello = "Hello World";
+    char hello[] = {'H','e','l','l','o',' ','W','o','r','l','d','!'};
+
+    for(char c : hello){
+        std::cout << c << std::endl;
+    }
     return 0;
 }
