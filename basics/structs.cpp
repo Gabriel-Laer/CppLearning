5f8cbdd7f262637441cd7b4b977978abf02aadf6
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-26 09:06:04 -0300

    primeira struct

--- basics/structs.cpp
+++ basics/structs.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <string>
 
+struct base {
+    char c;
+    int x;
+    int y;
+    double z;
+};
 int main(){
-    
+    base a;
+
+    a.c = 'H';
+    a.x = 10;
+    a.y = 12;
+    a.z = 12.25;
+
+    std::cout << a.c << a.x << a.y << a.z << std::endl;
     return 0;
 }
