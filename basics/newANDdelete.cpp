dfc1401fb766b45acd82d15ed0a0e78dffa48c0a
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-26 10:17:10 -0300

    finalização de new e delete

--- basics/newANDdelete.cpp
+++ basics/newANDdelete.cpp
@@ -2,6 +2,28 @@
 #include <string>
 
 int main(){
+    int* ptr = new int; // para variaveis
+    *ptr = 666;
+    int num = 0;
+
+    std::cout << ptr << std::endl;
+    std::cout << &ptr << std::endl;
+
+    std::cout << "Digite a quantidade de numeros no array: ";
+    std::cin >> num;
+
+    int* array = new int[num];
+
+    for (size_t i = 0; i < num; i++){
+        array[i] = i;
+    }
+    
+    for (size_t i = 0; i < num; i++){
+        std::cout << array[i] << std::endl;
+    }
+
+    delete ptr;
+    delete[] array;
 
     return 0;
 }
