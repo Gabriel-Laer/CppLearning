77d7d74c21510c451241c926792fc6fc1ea91121
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-28 09:11:28 -0300

    finalizando files

--- basics/files.cpp
+++ basics/files.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main(){
-    
+    std::ofstream myFile("nome.txt");
+    std::string nome;
+    myFile << "Gabriel Silva.";
+    myFile.close();
+
+    std::ifstream readFile("nome.txt");
+
+    while(getline(readFile,nome)){
+        std::cout << nome << std::endl;
+    }
+
+    readFile.close();
     return 0;
 }
