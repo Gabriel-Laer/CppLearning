8518520753815d2b2b98bfa496e640306961f82b
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-07-28 08:36:11 -0300

    finalizando

--- basics/oop.cpp
+++ basics/oop.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
 
+
+class Light{
+    public:
+        int lOff = 0;
+};
+
 int main(){
-    
+    Light light1;
+    std::cout << light1.lOff << std::endl;
+    light1.lOff = 1;
+    std::cout << light1.lOff << std::endl;
     return 0;
 }
