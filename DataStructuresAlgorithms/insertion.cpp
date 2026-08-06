9397367acc073fa1d6f554ba0b7400b02ab52140
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-08-06 08:20:05 -0300

    implementação completa

--- DataStructuresAlgorithms/insertion.cpp
+++ DataStructuresAlgorithms/insertion.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+void insertionSort(int* array, int size);
 
 int main(){
+    int a[] = {64, 34, 25, 12, 22, 11, 90, 5};
+    int size = sizeof(a)/sizeof(a[0]);
+
+    insertionSort(a,size);
+    for(int i : a){
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
     return 0;
+}
+
+void insertionSort(int* array, int size){
+    int insertIndex = 0;
+    int currentValue = 0;
+    int j = 0;
+    for (int i = 1; i < size; i++){
+        currentValue = array[i];
+        insertIndex = i;
+        j = i - 1;
+        while (j >= 0 && array[j] > currentValue){
+            array[j+1] = array[j];
+            insertIndex = j;
+            j--;
+        }
+        array[insertIndex] = currentValue;
+    }
+    
 }
