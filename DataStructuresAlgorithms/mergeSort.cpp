724375490f6f0eed1f13eab2388884004d913427
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-08-10 14:47:02 -0300

    implementando merge sort com recursão

---
+++ DataStructuresAlgorithms/mergeSort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+void mergeSort(std::vector<double> a);
+
+int main(){
+    std::vector<double> array = {3, 7, 6, -10, 15, 23.5, 55, -13};
+
+    return 0;
+}
+
+void mergeSort(std::vector<double> a)
