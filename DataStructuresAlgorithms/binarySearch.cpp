2a4a7dd3d671990eae4f2019eff5a5112a9a1fa7
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-08-11 08:32:56 -0300

    ...

--- DataStructuresAlgorithms/binarySearch.cpp
+++ DataStructuresAlgorithms/binarySearch.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int binarySearch(std::vector<int>& a, int target);
 
 int main(){
     std::vector<int> array = {8,5,12,3,7};
-    
+
     return 0;
 }
 
