85e3107384a066925b7995e7e4798f2b8a05c571
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-08-11 08:54:18 -0300

    finalizado a implementação

--- DataStructuresAlgorithms/binarySearch.cpp
+++ DataStructuresAlgorithms/binarySearch.cpp
@@ -5,11 +5,28 @@
 int binarySearch(std::vector<int>& a, int target);
 
 int main(){
-    std::vector<int> array = {8,5,12,3,7};
-
+    std::vector<int> array = {1,2,3,4,5,7};
+    int x = 7;
+    std::string result = ((binarySearch(array, x) == -1) ? "No found." : "Value found.");
+    std::cout << result << std::endl;
     return 0;
 }
 
 int binarySearch(std::vector<int>& a, int target){
+    int left = 0;
+    int right = a.size() - 1;
+    int mid = 0;
 
+    while(left <= right){
+        mid = (left + right)/2;
+        if(a[mid] == target){
+            return mid;
+        }
+        if(a[mid] < target){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+    return - 1;
 }
