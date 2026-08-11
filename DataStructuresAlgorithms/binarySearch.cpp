e9a6a947f0fee8fca2e17c2c03cd2823d2122ba3
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-08-11 08:32:15 -0300

    Implementação de binary search

---
+++ DataStructuresAlgorithms/binarySearch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int binarySearch(std::vector<int>& a, int target);
+
+int main(){
+    std::vector<int> array = {8,5,12,3,7};
+    
+    return 0;
+}
+
+int binarySearch(std::vector<int>& a, int target){
+
+}
