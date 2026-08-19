5ec56b34d0e451e02f4972bb69eb875e3cc2eaa5
Gabriel-Laer <gabrielmarqueslaer@gmail.com>
2026-08-19 15:18:36 -0300

    finalizada a implementação da tree AVL

--- DataStructuresAlgorithms/treeAVL.cpp
+++ DataStructuresAlgorithms/treeAVL.cpp
@@ -11,9 +11,25 @@ struct Node{
 Node* createNode(int key);
 int getHeight(Node* node);
 int getBalanceFactor(Node* node);
+Node* rightRotate(Node* y);
+Node* leftRotate(Node* x);
+Node* insert(Node* node, int key);
+void inOrder(Node* root);
+void freeTree(Node* root);
 
 int main(){
-
+    Node* root = nullptr;
+    
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 10);
+    root = insert(root, 25);
+    inOrder(root);
+    std::cout << std::endl;
+    freeTree(root);
+    root = nullptr;
     return 0;
 }
 
@@ -32,4 +48,89 @@ int getHeight(Node* node){
 
 int getBalanceFactor(Node* node){
     return (node == nullptr) ? 0 : getHeight(node->left) - getHeight(node->right);
+}
+
+Node* rightRotate(Node* y){
+    Node* x = y->left;
+    Node* tmp = x->right;
+
+    x->right = y;
+    y->left = tmp;
+
+    y->height = std::max(getHeight(y->left),getHeight(y->right)) + 1;
+    x->height = std::max(getHeight(x->left),getHeight(x->right)) + 1;
+
+    return x;
+}
+
+Node* leftRotate(Node* x){
+    Node* y = x->right;
+    Node* tmp = y->left;
+
+    y->left = x;
+    x->right = tmp;
+
+    x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
+    
+    return y;
+}
+
+Node* insert(Node* node, int key){
+    int balance = 0;
+
+    if(node == nullptr){
+        return createNode(key);
+    }
+
+    if(key < node->key){
+        node->left = insert(node->left,key);
+    }else if(key > node->key){
+        node->right = insert(node->right, key);
+    }else{
+        return node;
+    }
+
+    node->height = 1 + std::max(getHeight(node->left),getHeight(node->right));
+    balance = getBalanceFactor(node);
+
+    // caso Left Left
+    if(balance > 1 && key < node->left->key){
+        return rightRotate(node);
+    }
+
+    //caso Right Right
+    if(balance < -1 && key > node->right->key){
+        return leftRotate(node);
+    }
+
+    //caso Left Right
+    if(balance > 1 && key > node->left->key){
+        node->right = leftRotate(node->right);
+        return rightRotate(node);
+    }
+
+    //caso Right Left
+    if(balance < - 1 && key < node->right->key){
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+void inOrder(Node* root){
+    if(root != nullptr){
+        inOrder(root->left);
+        std::cout << root->key << " ";
+        inOrder(root->right);
+    }
+}
+
+void freeTree(Node* root){
+    if(root == nullptr) return;
+
+    freeTree(root->left);
+    freeTree(root->right);
+    delete root;
 }
